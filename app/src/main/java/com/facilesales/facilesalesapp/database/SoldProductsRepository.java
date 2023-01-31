00e100e44b55f00b75b56977fa05840346c6ebbf
Vincent Hlatshwayo <thevince30@gmail.com>
2023-01-31 06:56:31 +0200

    Project done

---
+++ app/src/main/java/com/facilesales/facilesalesapp/database/SoldProductsRepository.java
@@ -0,0 +1,2 @@
+package com.facilesales.facilesalesapp.database;public class SoldProductsRepository {
+}
---
+++ app/src/main/java/com/facilesales/facilesalesapp/pojos/HistoryViewAdapter.java
@@ -0,0 +1,2 @@
+package com.facilesales.facilesalesapp.pojos;public class HistoryViewAdapter {
+}
